60c64ceb24148f44553000def34d2581928a5287
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-09-09 19:46:24 +0600

    Brute force

---
+++ 11496 - Musical Loop.cpp
@@ -0,0 +1,103 @@
+/*
+//Author : Rezwanul Islam Maruf
+//AUST ,CSE
+//25-th batch..
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <cctype>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <map>
+#include <list>
+#include <cstdlib>
+#include <sstream>
+#include <set>
+
+#define FOR(i,n) for(i=1;i<=n;i++)
+#define REP(i,n) for(i=0;i<n;i++)
+#define init(i,a,n) for(i=0;i<n;i++)a[i]=i
+#define inf 1000000500
+#define ms0(a) memset(a,0,sizeof(a))
+#define msn(a,n) memset(a,n,sizeof(a))
+#define i64  long long
+#define u64 unsigned long long
+#define DB double
+#define LD long double
+#define eps 1e-10
+#define SQR(x) ((x)*(x))
+#define pb push_back
+#define all(x) sort(x.begin(),x.end())
+#define II ({int a; scanf(" %d", &a); a;})
+#define SS(s) scanf("%s",&s)
+#define DD ({DB a; scanf(" %lf", &a); a;})
+#define LL  ({i64  a; scanf(" %lld", &a); a;})
+#define LU  ({u64  a; scanf(" %llu", &a); a;})
+#define CS printf("Case %d:\n",cs++)
+#define PI 3.1415926535897932384626433832795
+#define NP next_permutation
+#define PP prev_permutation
+#define mp make_pair
+#define PII pair <int , int>
+
+
+#define FRD freopen("in.txt", "r", stdin)
+#define FWR freopen("out.txt", "w", stdout)
+
+
+
+int aComp(const void * a, const void * b){return (*(int*)a-*(int*)b );}
+int dComp(const void * a, const void * b){return (*(int*)b-*(int*)a );}
+//qsort (array,size, sizeof(int), aComp);
+
+
+//Debugger
+#define debug_matrix(mat,row,col)for(int i=0;i<row;i++){for(int j=0;j<col;j++) cout<<mat[i][j]<<" ";cout<<endl;}
+#define debug_val(x) cout<<x<<endl
+#define debug_arr(x,n){for(int i=0;i<=n;i++){if(i)cout<<" ";cout<<x[i];}cout<<endl;}
+#define forstl(i,n) for(__typeof((n).begin())i =(n).begin();i!=(n).end();i++)
+
+int cs=1;
+//
+
+using namespace std;
+#define mx 10005
+int a[mx];
+int main()
+{
+    int n,i,x;
+    while(scanf("%d",&n) && n)
+    {
+      REP(i,n) a[i] =  II ;
+
+      int cnt,up;
+      if(a[1]>a[0]) up = 0;
+      else up =1;
+      cnt = 1;
+      a[n] = a[0];
+      a[n+1] = a[1];
+      for(i=1;i<n;i++)
+      {
+        int nxt = a[i+1];
+        int prev = a[i];
+        if(up)
+        {
+            if(nxt>prev) cnt++,up = 0;
+        }
+        else
+        {
+            if(prev>nxt) cnt++,up = 1;
+        }
+      }
+      if(a[n+1]>a[n] && !up) cnt--;
+      if(a[n+1]<a[n] &&  up) cnt--;
+      cout<<cnt<<endl;
+    }
+    return 0;
+}
