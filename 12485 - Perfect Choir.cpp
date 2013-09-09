33acd7c20c3d9e3c90650ad08e62a3d3e22ba72a
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-09-09 19:47:10 +0600

    adhoc

---
+++ 12485 - Perfect Choir.cpp
@@ -0,0 +1,83 @@
+/*
+//Author : Rezwanul Islam Maruf
+//AUST ,CSE
+//25-th batch..
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <cctype>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <map>
+#include <list>
+#include <cstdlib>
+#include <sstream>
+#include <set>
+
+#define FOR(i,n) for(i=1;i<=n;i++)
+#define REP(i,n) for(i=0;i<n;i++)
+#define init(i,a,n) for(i=0;i<n;i++)a[i]=i
+#define inf 9999999
+#define ms0(a) memset(a,0,sizeof(a))
+#define msn(a,n) memset(a,n,sizeof(a))
+#define i64  long long
+#define u64 unsigned long long
+#define DB double
+#define LD long double
+#define eps 1e-10
+#define SQR(x) ((x)*(x))
+#define pb push_back
+#define all(x) sort(x.begin(),x.end())
+#define II ({int a; scanf(" %d", &a); a;})
+#define SS(s) scanf("%s",&s)
+#define DD ({DB a; scanf(" %lf", &a); a;})
+#define LL  ({i64  a; scanf(" %lld", &a); a;})
+#define LU  ({u64  a; scanf(" %llu", &a); a;})
+#define CS printf("Case %d:\n",cs++)
+#define PI 3.1415926535897932384626433832795
+#define NP next_permutation
+#define PP prev_permutation
+#define mp make_pair
+#define PII pair <int , int>
+
+
+#define FRD freopen("in.txt", "r", stdin)
+#define FWR freopen("out.txt", "w", stdout)
+
+int cs=1;
+
+int aComp(const void * a, const void * b){return (*(int*)a-*(int*)b );}
+int dComp(const void * a, const void * b){return (*(int*)b-*(int*)a );}
+//qsort (array,size, sizeof(int), aComp);
+
+
+//Debugger
+#define debug_matrix(mat,row,col)for(int i=0;i<row;i++){for(int j=0;j<col;j++) cout<<mat[i][j]<<" ";cout<<endl;}
+#define debug_val(x) cout<<x<<endl
+#define debug_arr(x,n){for(int i=0;i<=n;i++){if(i)cout<<" ";cout<<x[i];}cout<<endl;}
+#define forstl(i,n) for(__typeof((n).begin())i =(n).begin();i!=(n).end();i++)
+using namespace std;
+
+int main()
+{
+    i64 n,i,s,res,a[100005];
+    while(cin>>n)
+    {
+        s = res = 0;
+        REP(i,n) s+= a[i]=II;
+
+        if(s%n ) puts("-1");
+        else   {
+                 s/=n;
+                 REP(i,n) res += abs(s-a[i]);
+                 cout<< (res>>1)+1<<endl;
+        }
+    }
+}
+
