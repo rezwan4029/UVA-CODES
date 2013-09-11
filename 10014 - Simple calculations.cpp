aced5052170e4a6e8cd86c1c1c92f3f0257c4d1a
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-09-11 21:04:30 +0600

    adhoc

---
+++ 10014 - Simple calculations.cpp
@@ -0,0 +1,83 @@
+/*
+//Author : Rezwanul Islam Maruf
+//AUST ,CSE
+//25-th batch..
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <cctype>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <map>
+#include <list>
+#include <cstdlib>
+
+#define FOR(i,n) for(i=1;i<=n;i++)
+#define FORa(i,n) for(i=0;i<n;i++)
+#define init(i,a,n) for(i=0;i<n;i++)a[i]=i
+#define inf 9999999
+#define ms0(a) memset(a,0,sizeof(a))
+#define msn(a,n) memset(a,n,sizeof(a))
+#define LL long long
+#define LLU unsigned long long
+#define DB double
+#define LD long double
+#define eps 1e-10
+#define SQR(x) ((x)*(x))
+#define pb push_back
+#define all(x) sort(x.begin(),x.end())
+#define inpI ({int a; scanf(" %d", &a); a;})
+#define inpD ({DB a; scanf(" %lf", &a); a;})
+#define inpLL ({LL a; scanf(" %lld", &a); a;})
+#define CS printf("Case %d: ",cs++)
+#define PI 3.1415926535897932384626433832795
+#define NP next_permutation
+#define PP prev_permutation
+#define mp make_pair
+#define PII pair <int , int>
+
+#define FRD freopen("in.txt", "r", stdin)
+
+
+int aComp(const void * a, const void * b){return (*(int*)a-*(int*)b );}
+int dComp(const void * a, const void * b){return (*(int*)b-*(int*)a );}
+//qsort (array,size, sizeof(int), aComp);
+
+
+//Debugger
+#define debug_matrix(mat,row,col)for(int i=0;i<row;i++){for(int j=0;j<col;j++) cout<<mat[i][j]<<" ";cout<<endl;}
+#define debug_val(x) cout<<x<<endl
+#define debug_arr(x,n){for(int i=0;i<=n;i++){if(i)cout<<" ";cout<<x[i];}cout<<endl;}
+//
+
+using namespace std;
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[6]={2,1,-1,-2,-1,1};int dy[6]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+int main()
+{
+    int i,f=0,t=inpI;
+    DB c[3005];
+    while(t--)
+    {
+        if(f)cout<<endl;f=1;
+        int n=inpI;
+        DB a=inpD,b=inpD;
+        c[0]=0;
+
+        DB s=0;
+        for(i=1;i<=n;i++){c[i]=inpD;s+=c[i]+=c[i-1];}
+        DB ans= (a*n+b -2*s)/(n+1);
+        printf("%.2lf\n",ans);
+    }
+    return 0;
+}
+
