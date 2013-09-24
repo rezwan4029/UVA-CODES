15e817d597d96d052cfd34054b2adf4a89134823
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-09-24 10:58:25 +0600

    adhoc/Number theory

---
+++ 10176 - Ocean Deep ! - Make it shallow !!.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define INF 1<<29
+#define all(x) x.begin(),x.end()
+#define ms(a,v) memset(a,v,sizeof a)
+#define II ({int a; scanf("%d", &a); a;})
+#define LL  ({ll  a; scanf("%lld", &a); a;})
+#define EPS 1e-10
+#define pi acos(0.0)
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+#define rep(i,n) for( __typeof(n)i = 0 ; i < n ; i++)
+#define For(i,n) for( __typeof(n)i = 1 ; i <= n ; i++)
+#define forstl(i,n) for(__typeof(n.begin())i = n.begin();i!=n.end();i++)
+#define forab(i,a,b) for( __typeof(b) i = a ; i <= (b) ; i++ )
+#define forba(i,b,a) for( __typeof(b) i = b ; i >= (a) ; i-- )
+
+const int MOD = 131071 ;
+
+int main(){
+    char c ;
+    int M ;
+    while(cin >> c ){
+        M  = c - '0' ;
+        while( cin >> c ){
+            if( c == '#' ) break ;
+            M = M * 2 + c - '0' ;
+            M %= MOD ;
+        }
+        if(M) puts("NO");
+        else  puts("YES");
+    }
+}
