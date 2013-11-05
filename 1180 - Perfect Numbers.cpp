a4cffef54a9677c137fa42fb211158b2459a0862
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-11-05 19:00:37 +0600

    Number theory

---
+++ 1180 - Perfect Numbers.cpp
@@ -0,0 +1,45 @@
+/*
+    Rezwan_4029
+    AUST , CSE-25
+*/
+#include <bits/stdc++.h>
+#define pb push_back
+#define INF 1<<29
+#define all(x) x.begin(),x.end()
+#define ms(a,v) memset(a,v,sizeof a)
+#define II ({int a; scanf("%d", &a); a;})
+#define LL  ({ll  a; scanf("%lld", &a); a;})
+#define EPS 1e-12
+#define pi 3.1415926535897932384626433832795
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef vector<ll>vl;
+typedef pair<int,int>pii;
+typedef pair<ll,ll>pll;
+
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+
+int main()
+{
+    #ifdef LOCAL
+      freopen ("in.txt", "r", stdin);
+    #endif
+    int N = II ;
+    int p ;
+    rep(i,N)
+    {
+        scanf("%d,",&p);
+        if( p == 2 || p == 3 || p == 5 || p == 7 || p == 13 || p == 17 || p == 19 )puts("Yes") ;
+        else puts("No");
+    }
+}
