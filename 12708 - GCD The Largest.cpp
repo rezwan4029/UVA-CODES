11e38f23993bc134da215f6a000614e1ee286ceb
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-11-27 02:57:39 +0600

    adhoc

---
+++ 12708 - GCD The Largest.cpp
@@ -0,0 +1,39 @@
+/*
+    Rezwan_4029
+    AUST , CSE-25
+*/
+#include <bits/stdc++.h>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ms(a,v) memset(a,v,sizeof a)
+#define II ({int a; scanf("%d", &a); a;})
+#define LL  ({ll  a; scanf("%lld", &a); a;})
+#define EPS 1e-10
+#define pi 3.1415926535897932384626433832795
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef vector<ll>vl;
+typedef pair<int,int>pii;
+typedef pair<ll,ll>pll;
+typedef pair<double,double>pdd;
+
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+
+int main(){
+	int t = II ;
+	For(cs,t){
+		ull n ;
+		scanf("%llu",&n);
+		printf("%llu\n",n/2);
+	}
+}
