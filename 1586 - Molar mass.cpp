3761153d488ef7d03d9f1f2722cf28c903a18036
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-01-05 03:20:59 +0600

    adhoc

---
+++ 1586 - Molar mass.cpp
@@ -0,0 +1,122 @@
+/*
+    Rezwan_4029 , AUST
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ms(a,v) memset(a,v,sizeof a)
+#define II ({int a; scanf("%d", &a); a;})
+#define LL ({Long a; scanf("%lld", &a); a;})
+#define DD ({double a; scanf("%lf", &a); a;})
+#define ff first
+#define ss second
+#define mp make_pair
+#define EPS 1e-10
+#define pi 3.1415926535897932384626433832795
+using namespace std;
+
+#define FI freopen ("in.txt", "r", stdin)
+#define FO freopen ("out.txt", "w", stdout)
+
+typedef long long Long;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef set<int> si;
+typedef vector<Long>vl;
+typedef pair<int,int>pii;
+typedef pair<string,int>psi;
+typedef pair<Long,Long>pll;
+typedef pair<double,double>pdd;
+
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+#ifdef LOCAL
+#define __(args...) {dbg,args; cerr<<endl;}
+struct debugger{template<typename T> debugger& operator , (const T& v){cerr<<v<<"\t"; return *this; }}dbg;
+#define __1D(a,n) rep(i,n) { if(i) printf(" ") ; cout << a[i] ; }
+#define __2D(a,r,c,f) forab(i,f,r-!f){forab(j,f,c-!f){if(j!=f)printf(" ");cout<<a[i][j];}cout<<endl;}
+#endif
+template<class A, class B> ostream &operator<<(ostream& o, const pair<A,B>& p){ return o<<"("<<p.ff<<", "<<p.ss<<")";} //Pair print
+template<class T> ostream& operator<<(ostream& o, const vector<T>& v){ o<<"[";forstl(it,v)o<<*it<<", ";return o<<"]";} //Vector print
+template<class T> ostream& operator<<(ostream& o, const set<T>& v){ o<<"[";forstl(it,v)o<<*it<<", ";return o<<"]";} //Set print
+
+//Fast Reader
+template<class T>inline bool read(T &x){int c=getchar();int sgn=1;while(~c&&c<'0'||c>'9'){if(c=='-')sgn=-1;c=getchar();}for(x=0;~c&&'0'<=c&&c<='9';c=getchar())x=x*10+c-'0'; x*=sgn; return ~c;}
+
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+
+const int MX = 1e5 + 7 ;
+const int INF = 1e8 + 7;
+
+double toDouble(string s){
+    double r = 0 ;
+    istringstream sin(s); sin>>r;
+    return r;
+}
+
+char che[MX];
+
+
+int main(){
+#ifdef LOCAL
+         FI ;
+#endif
+    int test = II ;
+    double C = 12.01 , H = 1.008 , O = 16.00 , N = 14.01 ;
+    For(cs,test){
+        scanf("%s",&che);
+        bool flag = false ;
+        int len = strlen(che);
+        double Ans = 0 ;
+        rep(i,len){
+            if( che[i] == 'C' ){
+                int p = i + 1 ;
+                string r ;
+                while( isdigit( che[p]) && p < len ) r += che[p++];
+                i = p - 1 ;
+                double x = toDouble(r) ;
+                if( x == 0 ) x = 1 ;
+                Ans +=  ( C * x );
+            }
+            else if( che[i] == 'H' ){
+                int p = i + 1 ;
+                string r ;
+                while( isdigit( che[p]) && p < len ) r += che[p++];
+                i = p - 1 ;
+                double x = toDouble(r) ;
+                if( x == 0 ) x = 1 ;
+                Ans +=  ( H * x );
+            }
+            else if( che[i] == 'N' ){
+                int p = i + 1 ;
+                string r ;
+                while( isdigit( che[p]) && p < len ) r += che[p++];
+                i = p - 1 ;
+                double x = toDouble(r) ;
+                if( x == 0 ) x = 1 ;
+                Ans +=  ( N * x );
+            }
+            else if( che[i] == 'O' ){
+                int p = i + 1 ;
+                string r ;
+                while( isdigit( che[p]) && p < len ) r += che[p++];
+                i = p - 1 ;
+                double x = toDouble(r) ;
+                if( x == 0 ) x = 1 ;
+                Ans +=  ( O * x );
+            }
+        }
+        printf("%.3lf\n",Ans);
+    }
+}
