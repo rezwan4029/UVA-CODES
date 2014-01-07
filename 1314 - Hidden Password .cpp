995bd4908d39b1cd496d7f6671f899eab6b256f5
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-01-07 07:25:03 +0600

    Strings / Minimum rotation

---
+++ 1314 - Hidden Password .cpp
@@ -0,0 +1,82 @@
+/*
+    Rezwan_4029 , AUST
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ms(a,v) memset(a,v,sizeof a)
+#define II ({int a; scanf("%d", &a); a;})
+#define LL ({Long a; scanf("%lld", &a); a;})
+#define DD ({double a; scanf("%lf", &a); a;})
+#define ff first
+#define ss second
+#define mp make_pair
+#define EPS 1e-10
+#define pi 3.1415926535897932384626433832795
+using namespace std;
+
+#define FI freopen ("in.txt", "r", stdin)
+#define FO freopen ("out.txt", "w", stdout)
+
+typedef long long Long;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef set<int> si;
+typedef vector<Long>vl;
+typedef pair<int,int>pii;
+typedef pair<string,int>psi;
+typedef pair<Long,Long>pll;
+typedef pair<double,double>pdd;
+
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+#ifdef LOCAL
+#define __(args...) {dbg,args; cerr<<endl;}
+struct debugger{template<typename T> debugger& operator , (const T& v){cerr<<v<<"\t"; return *this; }}dbg;
+#define __1D(a,n) rep(i,n) { if(i) printf(" ") ; cout << a[i] ; }
+#define __2D(a,r,c,f) forab(i,f,r-!f){forab(j,f,c-!f){if(j!=f)printf(" ");cout<<a[i][j];}cout<<endl;}
+#endif
+template<class A, class B> ostream &operator<<(ostream& o, const pair<A,B>& p){ return o<<"("<<p.ff<<", "<<p.ss<<")";} //Pair print
+template<class T> ostream& operator<<(ostream& o, const vector<T>& v){ o<<"[";forstl(it,v)o<<*it<<", ";return o<<"]";} //Vector print
+template<class T> ostream& operator<<(ostream& o, const set<T>& v){ o<<"[";forstl(it,v)o<<*it<<", ";return o<<"]";} //Set print
+
+//Fast Reader
+template<class T>inline bool read(T &x){int c=getchar();int sgn=1;while(~c&&c<'0'||c>'9'){if(c=='-')sgn=-1;c=getchar();}for(x=0;~c&&'0'<=c&&c<='9';c=getchar())x=x*10+c-'0'; x*=sgn; return ~c;}
+
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+char s[100007];
+int N ;
+int min_rotation(){
+    int ans = 0,p = 1,len = 0;
+    while(p < N && ans + len + 1 < N){
+        if(s[ans + len] == s[(p + len) % N]) ++len;
+        else if(s[ans + len] < s[(p + len) % N]){
+            p = p + len + 1;
+            len = 0;
+        }else{
+            ans = max(ans + len + 1,p);
+            p = ans + 1;
+            len = 0;
+        }
+    }
+    return ans;
+}
+int main(){
+    int t ; read(t);
+    while( t-- ){
+        read(N);
+        scanf("%s",s);
+        printf("%d\n",min_rotation());
+    }
+}
