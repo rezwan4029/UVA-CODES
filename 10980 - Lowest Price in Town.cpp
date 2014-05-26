53313e4a7781789ab14e4bfa15b71e44e60d28d7
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-05-26 19:34:00 +0600

    Dynamic Programming / Knapsack / Coin Change

---
+++ 10980 - Lowest Price in Town.cpp
@@ -0,0 +1,113 @@
+/*
+    AUST.royal_flush
+*/
+
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ms(a,v) memset(a,v,sizeof a)
+#define II ({int a; scanf("%d", &a); a;})
+#define LL ({Long a; scanf("%lld", &a); a;})
+#define DD ({double a; scanf("%lf", &a); a;})
+#define ff first
+#define ss second
+#define mp make_pair
+#define gc getchar
+#define EPS 1e-10
+#define pi 3.1415926535897932384626433832795
+using namespace std;
+
+#define FI freopen ("in.txt", "r", stdin)
+#define FO freopen ("out.txt", "w", stdout)
+
+typedef long long Long;
+typedef long long int64;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef set<int> si;
+typedef vector<Long>vl;
+typedef pair<int,int>pii;
+typedef pair<string,int>psi;
+typedef pair<Long,Long>pll;
+typedef pair<double,double>pdd;
+typedef vector<pii> vpii;
+
+
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+#define __(args...) {dbg,args; cerr<<endl;}
+struct debugger{template<typename T> debugger& operator , (const T& v){cerr<<v<<"\t"; return *this; }}dbg;
+#define __1D(a,n) rep(i,n) { if(i) printf(" ") ; cout << a[i] ; }
+#define __2D(a,r,c,f) forab(i,f,r-!f){forab(j,f,c-!f){if(j!=f)printf(" ");cout<<a[i][j];}cout<<endl;}
+
+template<class A, class B> ostream &operator<<(ostream& o, const pair<A,B>& p){ return o<<"("<<p.ff<<", "<<p.ss<<")";} //Pair print
+template<class T> ostream& operator<<(ostream& o, const vector<T>& v){ o<<"[";forstl(it,v)o<<*it<<", ";return o<<"]";} //Vector print
+template<class T> ostream& operator<<(ostream& o, const set<T>& v){ o<<"[";forstl(it,v)o<<*it<<", ";return o<<"]";} //Set print
+template<class T> inline void MAX(T &a , T b){ if (a < b ) a = b;}
+template<class T> inline void MIN(T &a , T b){ if (a > b ) a = b;}
+
+//Fast Reader
+template<class T>inline bool read(T &x){int c=gc();int sgn=1;while(~c&&c<'0'||c>'9'){if(c=='-')sgn=-1;c=gc();}for(x=0;~c&&'0'<=c&&c<='9';c=gc())x=x*10+c-'0';x*=sgn;return ~c;}
+
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+int toInt(string s){
+    int r = 0 ;
+    istringstream sin(s); sin>>r;
+    return r;
+}
+
+vi Split(string s ){
+    vi ret;
+    istringstream ss(s) ;
+    string token ;
+    while(ss >> token) ret.pb( toInt(token) ) ;
+    return ret;
+}
+const double INF = 1e10;
+double price[27] ;
+short quantity[27];
+double dp[27][107];
+int Vis[27][107] , marker = 0 ;
+char str[12345];
+short N ;
+double solve(int pos , short Taken){
+    if( Taken <= 0 ) return 0 ;
+    if( pos == N + 1 ) return INF ;
+    double &ret = dp[pos][Taken];
+    if( Vis[pos][Taken] == marker ) return ret ;
+    Vis[pos][Taken] = marker ;
+    ret = INF ;
+    MIN( ret , solve(pos+1,Taken) );
+    MIN( ret , solve(pos,Taken-quantity[pos])+price[pos] );
+    return ret ;
+}
+int main() {
+    int cs = 1 ;
+    while( scanf("%lf %d",&price[0],&N) != EOF ){
+        quantity[0] = 1 ;
+        For(i,N){
+            read(quantity[i]);
+            scanf("%lf",&price[i]);
+        }
+        getchar();
+        gets(str);
+        vi Query = Split(str);
+        printf("Case %d:\n",cs++);
+        forstl(it,Query){
+            marker++;
+            double Ans = solve(0,*it);
+            printf("Buy %d for $%.2lf\n",*it,Ans);
+        }
+    }
+}
