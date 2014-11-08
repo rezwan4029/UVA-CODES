3140577c91b7e97b18048cfe12a5512105fcc55c
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-11-08 09:40:40 +0600

    Simulation / PairSum / Math

---
+++ 10202 - Pairsumonious Numbers.cpp
@@ -0,0 +1,141 @@
+/*
+   Team - icpc2014dhaka-36 , AUST_toothless
+*/
+
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ms(a,v) memset(a,v,sizeof a)
+#define II ( { int a ; read(a) ; a; } )
+#define LL ( { Long a ; read(a) ; a; } )
+#define DD ({double a; scanf("%lf", &a); a;})
+#define ff first
+#define ss second
+#define mp make_pair
+
+#define pi 3.1415926535897932384626433832795
+using namespace std;
+
+typedef long long Long;
+typedef long long int64;
+typedef unsigned long long ull;
+
+typedef pair<int,int>pii;
+typedef pair<string,int>psi;
+typedef pair<Long,Long>pll;
+typedef pair<double,double>pdd;
+
+typedef vector<int>    vi;
+typedef vector<Long>   vl;
+typedef vector<string> vs;
+typedef vector<pii>  vpii;
+typedef set<int> si;
+
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+
+#define __( args... ) { dbg , args ; cerr << endl; }
+
+struct debugger {
+    template< typename T > debugger & operator , ( const T &v ) {
+        cerr << v << "\t" ;
+        return *this;
+    }
+}dbg;
+// Pair Print
+template< class A , class B > ostream &operator << ( ostream &o, const pair<A,B> &p ) {
+    return o<<"(" << p.ff << ", " << p.ss << ")" ;
+}
+//Vector Print
+template<class T> ostream& operator<<(ostream& o, const vector<T>& v) {
+    o << "[";
+    forstl(it,v) o << *it << ", " ;
+    return o << "]";
+}
+//Set Print
+template<class T> ostream& operator<<(ostream& o, const set<T>& v) {
+    o << "[";
+    forstl(it,v) o << *it << ", " ;
+    return o << "]";
+}
+template<class T> inline void MAX( T &a , T b) { if (a < b ) a = b; }
+template<class T> inline void MIN( T &a , T b) { if (a > b ) a = b; }
+
+//Fast Reader
+template< class T > inline bool read( T &x ) {
+    int c = getchar() ;
+    int sgn = 1;
+    while( ~c && c < '0' || c > '9' ) {
+        if( c == '-' )sgn = -1 ;
+        c = getchar() ;
+    }
+    for( x = 0 ; ~c && '0' <= c && c <= '9' ; c = getchar() ) x = x * 10 + c - '0' ;
+    x *= sgn ;
+    return ~c ;
+}
+
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+bool pairsums( int *ans, multiset< int > &seq )
+{
+    int N = seq.size();
+    if( N < 3 ) return false;
+    __typeof( seq.end() ) it = seq.begin();
+    int a = *it++, b = *it++, i = 2;
+
+    for( ; i * ( i - 1 ) < 2 * N && it != seq.end(); i++, ++it )
+    {
+        // assume seq[i] = ans[1] + ans[2]
+        ans[0] = a + b - *it;
+        if( ans[0] & 1 ) continue;
+        ans[0] >>= 1;
+
+        // try ans[0] as a possible least element
+        multiset< int > seq2 = seq;
+        int j = 1;
+        while( seq2.size() )
+        {
+            ans[j] = *seq2.begin() - ans[0];
+            for( int k = 0; k < j; k++ )
+            {
+                __typeof( seq2.end() ) jt = seq2.find( ans[k] + ans[j] );
+                if( jt == seq2.end() ) goto hell;
+                seq2.erase( jt );
+            }
+            j++;
+        }
+        hell:;
+        if( j * ( j - 1 ) < 2 * N ) continue;
+        return true;
+    }
+    return false;
+}
+
+int main() {
+  //  freopen("cin.txt","r",stdin);
+    int n ;
+    while( cin >> n ){
+        multiset< int > s;
+        int m = n * ( n - 1 ) / 2 ;
+        rep(i,m) s.insert( II );
+        int ans[m+1];
+        bool hasSolution = pairsums( ans, s );
+        if( hasSolution ){
+            rep(i,n) {
+                if(i) printf(" ");
+                printf("%d",ans[i]);
+            }
+            puts("");
+        }else puts("Impossible");
+    }
+}
