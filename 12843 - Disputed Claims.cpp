5c6560f8ffa08e4d1d661501448f11521b182182
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-11-08 11:03:42 +0600

    adhoc / Geometry / Math / Sorting

---
+++ 12843 - Disputed Claims.cpp
@@ -0,0 +1,134 @@
+/*
+    AUST_toothless
+*/
+
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ms(a,v) memset(a,v,sizeof a)
+#define II ( { int a ; read(a) ; a; } )
+#define LL ( { Long a ; read(a) ; a; } )
+#define DD ({double a; scanf("%lf", &a); a;})
+#define ff first
+#define ss second
+#define mp make_pair
+
+#define pi 3.1415926535897932384626433832795
+using namespace std;
+
+typedef long long Long;
+typedef long long int64;
+typedef unsigned long long ull;
+
+typedef pair<int,int>pii;
+typedef pair<string,int>psi;
+typedef pair<Long,Long>pll;
+typedef pair<double,double>pdd;
+
+typedef vector<int>    vi;
+typedef vector<Long>   vl;
+typedef vector<string> vs;
+typedef vector<pii>  vpii;
+typedef set<int> si;
+
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+
+#define __( args... ) { dbg , args ; cerr << endl; }
+
+struct debugger {
+    template< typename T > debugger & operator , ( const T &v ) {
+        cerr << v << "\t" ;
+        return *this;
+    }
+}dbg;
+// Pair Print
+template< class A , class B > ostream &operator << ( ostream &o, const pair<A,B> &p ) {
+    return o<<"(" << p.ff << ", " << p.ss << ")" ;
+}
+//Vector Print
+template<class T> ostream& operator<<(ostream& o, const vector<T>& v) {
+    o << "[";
+    forstl(it,v) o << *it << ", " ;
+    return o << "]";
+}
+//Set Print
+template<class T> ostream& operator<<(ostream& o, const set<T>& v) {
+    o << "[";
+    forstl(it,v) o << *it << ", " ;
+    return o << "]";
+}
+template<class T> inline void MAX( T &a , T b) { if (a < b ) a = b; }
+template<class T> inline void MIN( T &a , T b) { if (a > b ) a = b; }
+
+//Fast Reader
+template< class T > inline bool read( T &x ) {
+    int c = getchar() ;
+    int sgn = 1;
+    while( ~c && c < '0' || c > '9' ) {
+        if( c == '-' )sgn = -1 ;
+        c = getchar() ;
+    }
+    for( x = 0 ; ~c && '0' <= c && c <= '9' ; c = getchar() ) x = x * 10 + c - '0' ;
+    x *= sgn ;
+    return ~c ;
+}
+
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+struct node{
+    int a,b,c;
+    node(int a,int b,int c):a(a),b(b),c(c){}
+};
+
+bool cmp(node x,node y){
+    if(x.a == y.a){
+        if(x.b == y.b){
+            return x.c < y.c;
+        }
+        return x.b<y.b;
+    }
+    return x.a < y.a;
+}
+
+void call(int A){
+    int sz = sqrt(A);
+    vpii vec;
+    For(i,sz){
+        if(A%i == 0) vec.pb(pii(i,A/i));
+    }
+    int cnt = 0;
+    vector<node>ans;
+    forstl(it,vec){
+        Long a = it->ff , b = it->ss;
+        Long sqab = a*a + b*b;
+        Long c = sqrt(sqab);
+        if(c*c == sqab){
+            ans.pb(node(a,b,c));
+        }
+    }
+    sort(all(ans),cmp);
+    printf("%d\n",ans.size());
+    forstl(it,ans){
+        printf("(%d,%d,%d)\n",(*it).a,(*it).b,(*it).c);
+    }
+}
+int main() {
+   // freopen("bin.txt","r",stdin);
+    int T = II ;
+    For(cs,T){
+        int A = II;
+        printf("Case %d: ",cs);
+        call(A*2);
+    }
+}
