9a3f446eddb1efbc972e8965f60380d06d4df694
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2015-07-11 10:50:48 +0600

    Geometry / Circle

---
+++ 12921 - Triple shot.cpp
@@ -0,0 +1,95 @@
+/*
+    Rezwan_4029 , AUST
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ms(a,v) memset(a,v,sizeof a)
+#define makeUnique(a) a.resize(unique(a.begin(), a.end()) - a.begin())
+#define II ({int a; scanf("%d", &a); a;})
+#define LL ({Long a; scanf("%lld", &a); a;})
+#define DD ({double a; scanf("%lf", &a); a;})
+#define ff first
+#define ss second
+#define mp make_pair
+#define gc getchar
+#define EPS 1e-10
+#define pi 3.1415926535897932384626433832795
+using namespace std;
+
+#define FI freopen ("in.txt", "r", stdin)
+#define FO freopen ("out.txt", "w", stdout)
+
+typedef long long Long;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef set<int> si;
+typedef vector<Long>vl;
+typedef pair<int,int>pii;
+typedef pair<string,int>psi;
+typedef pair<Long,Long>pll;
+typedef pair<double,double>pdd;
+typedef vector<pii> vpii;
+
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+#define __(args...) {dbg,args; cerr<<endl;}
+struct debugger{template<typename T> debugger& operator , (const T& v){cerr<<v<<"\t"; return *this; }}dbg;
+#define __1D(a,n) rep(i,n) { if(i) printf(" ") ; cout << a[i] ; }
+#define __2D(a,r,c,f) forab(i,f,r-!f){forab(j,f,c-!f){if(j!=f)printf(" ");printf("%2d",a[i][j]);}cout<<endl;}
+
+template<class A, class B> ostream &operator<<(ostream& o, const pair<A,B>& p){ return o<<"("<<p.ff<<", "<<p.ss<<")";} //Pair print
+template<class T> ostream& operator<<(ostream& o, const vector<T>& v){ o<<"[";forstl(it,v)o<<*it<<", ";return o<<"]";} //Vector print
+template<class T> ostream& operator<<(ostream& o, const set<T>& v){ o<<"[";forstl(it,v)o<<*it<<", ";return o<<"]";} //Set print
+template<class T> inline void MAX(T &a , T b){ if (a < b ) a = b;}
+template<class T> inline void MIN(T &a , T b){ if (a > b ) a = b;}
+
+//Fast Reader
+template<class T>inline bool read(T &x){int c=gc();int sgn=1;while(~c&&c<'0'||c>'9'){if(c=='-')sgn=-1;c=gc();}for(x=0;~c&&'0'<=c&&c<='9';c=gc())x=x*10+c-'0';x*=sgn;return ~c;}
+
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+struct point{
+    double x , y ;
+    point(double _x, double _y){
+        x = _x , y =_y;
+    }
+    point(){}
+};
+
+bool CircleThroughThreePoints(point p1, point p2, point p3, point &center){
+    double a,b,c,d,e,f,g;
+    a = p2.x - p1.x ;  b = p2.y - p1.y ;
+    c = p3.x - p1.x ;  d = p3.y - p1.y ;
+    e = a*(p1.x + p2.x) + b*(p1.y + p2.y) ;
+    f = c*(p1.x + p3.x) + d*(p1.y + p3.y) ;
+    g = 2.0 * ( a*(p3.y - p2.y) - b*( p3.x - p2.x ) );
+    if (fabs(g) < EPS) return false;
+    //__((d*e - b*f) / g, (a*f - c*e) / g)
+    center = point ( (d*e - b*f) / g , (a*f - c*e) / g ) ;
+    return true;
+}
+
+int main(){
+    point p1 , p2 , p3 , center;
+    while( scanf("%lf %lf",&p1.x, &p1.y) != EOF ){
+        scanf("%lf %lf",&p2.x, &p2.y);
+        scanf("%lf %lf",&p3.x, &p3.y);
+        bool ok = CircleThroughThreePoints(p1, p2, p3, center);
+        if(!ok){
+            puts("Impossible");
+            continue;
+        }
+        printf("%.6lf %.6lf\n",center.x, center.y);
+    }
+}
