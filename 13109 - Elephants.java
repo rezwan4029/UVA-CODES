62cbb2764c602843c55d1a819eeac19b4fa2f656
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2016-12-13 11:18:27 +0600

    adhoc / sorting

---
+++ 13109 - Elephants.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String args[]) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		Task solver = new Task();
+		int T = in.nextInt();
+		for (int cs = 1; cs <= T; cs++) {
+			solver.solve(cs, in, out);
+		}
+		out.close();
+	}
+
+	static class Task {
+		public void solve(int testNumber, InputReader in, PrintWriter out) {
+			int M = in.nextInt();
+			int W = in.nextInt();
+			int elephants[] = new int[M];
+			for (int i = 0; i < M; i++) {
+				elephants[i] = in.nextInt();
+			}
+			Arrays.sort(elephants);
+			int currentWeight = 0, count = 0;
+			for (int i = 0; i < M; i++) {
+				if (currentWeight + elephants[i] > W) {
+					break;
+				}
+				count++;
+				currentWeight += elephants[i];
+			}
+			out.println(count);
+		}
+	}
+
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
+
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+	}
+}
